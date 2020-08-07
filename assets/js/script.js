41b4693153ed6956057092e8f63d0f6788006d4d
mayasaffron <mayasaffronhan@hotmail.com>
2020-08-07 19:37:06 +0000

    added more js to the images on my pt page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,24 @@
-$(document).ready(function () {
-  $(".house").click(function () {
-      // over
-      $(".revealText").fadeIn(3000);
-    });
 
-  $(".house").click(function () {
-      // over
-      $(".revealText").fadeOut();
-    });
+$(document).ready(function(){
 
-    $("nav").click(function(){
-        $("ul").toggleClass(dropdown);
+  $(".house").mouseenter(function () { 
+      $(".revealText").show();
+  $(".house").fadeTo(1000, 0.15);
 
-    })
+ 
 
+ $(".house").mouseLeave(function(){
+  $(".revealText").hide();
+
+});
+ 
+ $(".virtual").mouseenter(function () { 
+      $(".revealText").show();
+  $(".virtual").fadeTo(1000, 0.15);
 
 });
 
 
+});
+
+ });
