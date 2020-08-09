4e54e56cd8bc0f2bdc2a79e0533a20278656b162
mayasaffron <mayasaffronhan@hotmail.com>
2020-08-09 21:51:56 +0000

    added the email form javascript tag to each pt page

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -11,4 +11,22 @@ function sendMail(contactForm){
    function(error){
        console.log("FAILED", error);
    });
+
+emailjs.send("gmail","podcast_form",{
+    "from_name":contactForm.name.value,
+    "from_email":contactForm.emailaddress.value,
+    "editing_enquiry":contactForm.projectsummary.value,
+    "guest_enquiry":contactForm.projectsummary.value,
+    "mentoring_enquiry":contactForm.projectsummary.value,
+    "other_enquiry":contactForm.projectsummary.value
+})
+.then(
+        function(response){
+            console.log("SUCCESS", response);
+        },
+   function(error){
+       console.log("FAILED", error);
+   });
+
+
 }
