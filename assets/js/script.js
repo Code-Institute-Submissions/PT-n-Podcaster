b5c8d1f4a860ed6b7ce97a9b5d99a5a7105a8b52
mayasaffron <mayasaffronhan@hotmail.com>
2020-08-11 15:51:39 +0000

    added the review box js code to script.js, not working properly yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,4 +34,48 @@ $(".virtual-container").mouseleave(function(){
    
 });
 
+$(".fa-star").mouseover(function () { 
+$("#1one","#2one","#3one","4one","#5one").css("color", "orange");
+    
+});
+
+$("starmark").on("click" , function () { 
+    let count;
+
+function starmark(item)
+{
+count=item.id[0];
+sessionStorage.starRating = count;
+let subid= item.id.substring(1);
+for(let i=0;i<5;i++)
+{
+if(i<count)
+{
+document.getElementById((i+1)+subid).style.color="orange";
+}
+else
+{
+document.getElementById((i+1)+subid).style.color="black";
+}
+
+
+}
+
+}
+
+function result()
+{
+//Rating : Count
+//Review : Comment(id)
+alert("Rating : "+count+"\nReview : "+document.getElementById("comment").value);
+}
+
+$("#1one","#2one","#3one","4one","#5one").css("color", "orange");
+    
+});
+
+
+
+
+
  });
