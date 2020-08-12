196349809ecebbbad9dbe1f1bca170f4b98696fd
mayasaffron <mayasaffronhan@hotmail.com>
2020-08-12 10:28:30 +0000

    solved the contact form issue, will add sending success message

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,32 +1,37 @@
-function sendMail(contactForm){
-    emailjs.send("gmail" , "pt_form", {
-        "from_name":contactForm.name.value,
-        "from_email":contactForm.emailaddress.value,
-        "pt_enquiry":contactForm.projectsummary.value
+function sendPtFormMail(contactForm) {
+  emailjs
+    .send("gmail", "pt_form", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.emailaddress.value,
+      pt_enquiry: contactForm.projectsummary.value,
     })
     .then(
-        function(response){
-            console.log("SUCCESS", response);
-        },
-   function(error){
-       console.log("FAILED", error);
-   });
+      function (response) {
+        console.log("SUCCESS", response);
+        alert('Thank you');
+      },
+      function (error) {
+        console.log("FAILED", error);
+      }
+    );
+}
 
-emailjs.send("gmail","podcast_form",{
-    "from_name":contactForm.name.value,
-    "from_email":contactForm.emailaddress.value,
-    "editing_enquiry":contactForm.projectsummary.value,
-    "guest_enquiry":contactForm.projectsummary.value,
-    "mentoring_enquiry":contactForm.projectsummary.value,
-    "other_enquiry":contactForm.projectsummary.value
-})
-.then(
-        function(response){
-            console.log("SUCCESS", response);
-        },
-   function(error){
-       console.log("FAILED", error);
-   });
-
-
+function sendPodcastMail(contactForm) {
+  emailjs
+    .send("gmail", "podcast_form", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.emailaddress.value,
+      editing_enquiry: contactForm.projectsummary.value,
+      guest_enquiry: contactForm.projectsummary.value,
+      mentoring_enquiry: contactForm.projectsummary.value,
+      other_enquiry: contactForm.projectsummary.value,
+    })
+    .then(
+      function (response) {
+        console.log("SUCCESS", response);
+      },
+      function (error) {
+        console.log("FAILED", error);
+      }
+    );
 }
