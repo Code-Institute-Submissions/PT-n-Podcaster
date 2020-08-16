0137362949a3269b3eebecb58e65f0e483e008a3
mayasaffron <mayasaffronhan@hotmail.com>
2020-08-16 18:06:02 +0000

    used js to insure that when user hovers over session pack, corresponding rate illuminates. Added feature to corresponding pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,6 +35,51 @@ $(".remoteContainer").mouseover(function(){
 $(".remoteContainer").mouseleave(function(){
     $(".remote").slideUp("slow");
 });
+
+
+$("#session1").mouseover(function(){
+    $("#cost1").css("background-color", "#4A4930");
+    $("#cost1").css("color", "#fafafa" );
+});
+
+$("#session1").mouseleave(function(){
+    $("#cost1").css("background-color", "#CAC9AE");
+    $("#cost1").css("color", "black");
+});
+
+
+$("#session2").mouseover(function(){
+    $("#cost2").css("background-color", "#4A4930");
+    $("#cost2").css("color", "#fafafa" );
+});
+
+$("#session2").mouseleave(function(){
+    $("#cost2").css("background-color", "#CAC9AE");
+    $("#cost2").css("color", "black");
+});
+
+
+$("#session3").mouseover(function(){
+    $("#cost3").css("background-color", "#4A4930");
+    $("#cost3").css("color", "#fafafa" );
+});
+
+$("#session3").mouseleave(function(){
+    $("#cost3").css("background-color", "#CAC9AE");
+    $("#cost3").css("color", "black");
+});
+
+$("#session4").mouseover(function(){
+    $("#cost4").css("background-color", "#4A4930");
+    $("#cost4").css("color", "#fafafa" );
+});
+
+$("#session4").mouseleave(function(){
+    $("#cost4").css("background-color", "#CAC9AE");
+    $("#cost4").css("color", "black");
+});
+
+
 //not working
 
 const starRating=document.getElementById("starRatings").children;
