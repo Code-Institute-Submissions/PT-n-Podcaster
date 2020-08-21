e2a6cea2910cc039f252774aae8091395e1e8d1c
mayasaffron <mayasaffronhan@hotmail.com>
2020-08-21 08:56:04 +0000

    removed the sticky-top class from each page

--- assets/js/script.js
+++ assets/js/script.js
@@ -162,8 +162,13 @@ $('#footerTerms').on('click', function () {
   console.log();
 });
 
-
-
+const toTop=document.querySelector(".toTop");
+window.addEventListener("scroll" , () => {
+    if (window.pageYOffset>100){
+        toTop.classList.add("active");
+    } else {toTop.classList.remove("active");
+}
+})
 
     });
 
